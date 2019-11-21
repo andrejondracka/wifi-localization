7505a1347255dba70d6523f6982224d47b14e1ac
Andrej Ondracka <andrej.ondracka@gmail.com>
2019-11-21 17:44:37 +0100

    added new codes

--- code.R
+++ code.R
@@ -15,7 +15,7 @@ traindata_loc <- traindata[,1:520]
 
 
 
-traindata_loc[traindata_loc == 100] = -105
+traindata_loc[traindata_loc == 100] = -1000
 
 ###pca
 traindata_loc <- as.data.frame(t(scale(t(traindata_loc), center = T, scale = T)))
@@ -27,7 +27,7 @@ pcatest <- prcomp(traindata_loc, scale. = F)
 plot(pcatest$sdev/sum(pcatest$sdev))
 sum(pcatest$sdev[1:100]/sum(pcatest$sdev))
 
-plot(pcatest$x[,1], pcatest$x[,2], col = (traindata$BUILDINGID + 1))
+plot(pcatest$x[,1], pcatest$x[,3], col = traindata$LATITUDE)
 
 
 
@@ -35,7 +35,7 @@ set.seed(66)
 trainIndex <- createDataPartition(traindata$BUILDINGID, p = 0.7, list = FALSE)
 
 ####pca'd model
-pcad_data50 <- data.frame(pcatest$x[,1:100])
+pcad_data50 <- data.frame(pcatest$x[,1:50])
 pcad_d50_train <- pcad_data50[trainIndex, ]
 pcad_d50_test <- pcad_data50[-trainIndex, ]
 
@@ -62,7 +62,7 @@ rfmodels_pca <- lapply(pcad_traindatasets, function(x) train(i ~ ., x, method = 
 knnneighbors_pca <- lapply(pcad_totaltraindatasets, function(x) train(i ~ ., x, method = 'knn'))
 
 preds_rf_pca <- lapply(rfmodels_pca, function(x) predict(x, pcad_d50_test))
-preds_knn_pca <- lapply(knnneighbors_pca, function(x) predict(x, pcad_d50_test))
+preds_knn_pca <- lapply(knnneighbors_pca, function(x) predict(x, valdata_pcd))
 ###
 
 ###metrics of the pca model -- rf
@@ -77,13 +77,19 @@ plot(preds_rf_pca$LATITUDE, traindata_test$LATITUDE)
 
 
 ###metrics of the pca model -- knn
-confusionMatrix(preds_knn_pca$FLOOR, traindata_test$FLOOR)
+confusionMatrix(preds_knn_pca$FLOOR, valdata$FLOOR)
+confusionMatrix(preds_knn_pca$BUILDINGID, valdata$BUILDINGID)
 
-Metrics::rmse(preds_knn_pca$LONGITUDE, traindata_test$LONGITUDE)
-plot(preds_knn_pca$LONGITUDE, traindata_test$LONGITUDE)
 
-Metrics::rmse(preds_knn_pca$LATITUDE, traindata_test$LATITUDE)
-plot(preds_knn_pca$LATITUDE, traindata_test$LATITUDE)
+Metrics::mae(preds_knn_pca$LONGITUDE, valdata$LONGITUDE)
+cor(preds_knn_pca$LONGITUDE, valdata$LONGITUDE)^2
+
+plot(preds_knn_pca$LONGITUDE, valdata$LONGITUDE)
+
+Metrics::mae(preds_knn_pca$LATITUDE, valdata$LATITUDE)
+cor(preds_knn_pca$LATITUDE, valdata$LATITUDE)^2
+
+plot(preds_knn_pca$LATITUDE, valdata$LATITUDE)
 
 
 save (rfmodels_pca, knnneighbors_pca, file = 'rf_knn_pca_models.RData')
@@ -99,7 +105,7 @@ h2o.init()
 h2o_traindatasets <- list()
 h2o_testdatasets <- list()
 h2o_valdatasets <- list()
-h2o_totaltraindataset <- list()
+h2o_totaltraindatasets <- list()
 
 #for (i in c('LONGITUDE', 'LATITUDE', 'FLOOR')) {
 #  h2o_traindatasets[[i]] <- as.h2o(cbind(pcad_d50_train, i = traindata_train[,i]))
@@ -120,14 +126,17 @@ h2o_rfs_val <- map2 (h2o_totaltraindatasets, h2o_valdatasets,
 h2o_gbm_val <- map2 (h2o_totaltraindatasets, h2o_valdatasets, 
                     function(x,y) h2o.gbm (y = 'i', training_frame = x, validation_frame = y, max_depth = 30))
 
+h2o_svm_xgb <- map2 (h2o_totaltraindatasets, h2o_valdatasets, 
+                     function(x,y) h2o.xgboost (y = 'i', training_frame = x, validation_frame = y))
 
 
-h2o_val_predicts <- map2(h2o_gbm_val, h2o_valdatasets, function(x,y) h2o.predict(x,y))
+
+h2o_val_predicts <- map2(h2o_svm_xgb, h2o_valdatasets, function(x,y) h2o.predict(x,y))
 
 Metrics::rmse(valdata$LONGITUDE, as.vector(h2o_val_predicts$LONGITUDE))
 confusionMatrix(factor(valdata$FLOOR), h2o_val_predicts$FLOOR)
 
-plot(valdata$LATITUDE, as.vector(h2o_val_predicts$LATITUDE), col = valdata$FLOOR)
+plot(valdata$LONGITUDE, as.vector(h2o_val_predicts$LONGITUDE), col = valdata$FLOOR)
 
 
 plot(valdata$LATITUDE, as.vector(h2o_val_predicts$LATITUDE))
--- validationset.R
+++ validationset.R
@@ -2,16 +2,18 @@ valdata <- read.csv('validationData.csv')
 
 ###take the data and transform it by removing the useless routers from train set and plus transform using the same components
 valdata$FLOOR <- factor(valdata$FLOOR)
+valdata$BUILDINGID <- factor(valdata$BUILDINGID)
+
 
 valdata_loc <- valdata[,1:520]
-valdata_loc[valdata_loc == 100] = -105
+valdata_loc[valdata_loc == 100] = -1000
 #valsds <- apply(valdata_loc, 2, sd)
 
 valdata_loc_cs <- t(scale(t(valdata_loc)))
 valdata_loc_cs[is.nan(valdata_loc_cs)] = 0
 
 valdata_pcd <-valdata_loc_cs %*% pcatest$rotation
-valdata_pcd <- as.data.frame(valdata_pcd[,1:100])
+valdata_pcd <- as.data.frame(valdata_pcd[,1:50])
 
 ###train dataset model prediction
 
